5ab15f87334e2f85e40b7caadea9ee43c1fbf5bc
SergeyGrechikhin <blackblock456@gmail.com>
2025-05-27 19:13:42 +0200

    my homework14

---
+++ src/homework14/task2/Greeter.java
@@ -0,0 +1,11 @@
+package homework14.task2;
+
+public class Greeter {
+    public void greet (String name,int counter) {
+        for (int i = 0; i < counter; i++) {
+            System.out.println(" Hallo , " + name + " ! ");
+
+        }
+
+    }
+}
