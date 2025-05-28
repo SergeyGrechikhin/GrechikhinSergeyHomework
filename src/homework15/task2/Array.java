027a7b083d7038b42cc1a757cee82a61a5062941
SergeyGrechikhin <blackblock456@gmail.com>
2025-05-28 21:42:32 +0200

    myhomework15

---
+++ src/homework15/task2/Array.java
@@ -0,0 +1,44 @@
+package homework15.task2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("Задание №2.\n" +
+                "\n" +
+                "1.\tСоздайте массив из 5 случайных целых чисел из интервала [10;99]\n" +
+                "2.\tВыведите его на консоль в строку.\n" +
+                "3.\tОпределите и выведите на экран сообщение о том, является ли массив строго возрастающей последовательностью.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сколько чисел должно быть в массиве! ");
+        int counter = scanner.nextInt();
+        int[] myarray = new int[counter] ;
+        boolean isseries = true ;
+
+        for (int i = 0; i < myarray.length; i++) {
+            System.out.println("Введите ваше число");
+            myarray[i] = scanner.nextInt();
+
+        }
+        System.out.println("Ваш массив");
+        System.out.println(Arrays.toString(myarray));
+
+        for (int i = 0; i < myarray.length-1; i++) {
+            if (myarray[i] >= myarray[i+1])  {
+                isseries = false;
+                break;
+
+            }
+        }
+
+        if (isseries) {
+            System.out.println("Ваши числа в массиве идут по возврастанию ");
+        } else {
+            System.out.println("Ваши числа в массиве идут не по возврастанию");
+        }
+
+
+    }
+
+}
---
+++ src/homework15/task3/Product.java
@@ -0,0 +1,27 @@
+package homework15.task3;
+
+public class Product {
+    private String name;
+    private String price;
+    private String quantity;
+
+    public Product(String name, String quantity, String price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+
+}
