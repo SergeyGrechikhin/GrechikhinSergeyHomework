f1a26b5a9ea519a90b0325dceb3f9c79b271acef
SergeyGrechikhin <blackblock456@gmail.com>
2025-07-31 16:15:01 +0200

    14 Homework

--- src/javapro/homework14/task9/OrderDemo.java
+++ src/javapro/homework14/task9/OrderDemo.java
@@ -9,10 +9,10 @@ public class OrderDemo {
     public static void main(String[] args) {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order("Товар 1","Отправлен",1,"13.06.25",105,"Новинка"));
-        orders.add(new Order("Товар 2","В обработке",1,"20.07.25",115,"Одежда"));
-        orders.add(new Order("Товар 3","Отправлен",1,"20.07.25",110,"Запчасти"));
-        orders.add(new Order("Товар 4","В обработке",1,"20.07.25",120,"Игрушки"));
-        orders.add(new Order("Товар 5","Получен",1,"13.06.25",150,"Книги"));
+        orders.add(new Order("Товар 2","В обработке",2,"20.07.25",115,"Одежда"));
+        orders.add(new Order("Товар 3","Отправлен",3,"20.07.25",110,"Запчасти"));
+        orders.add(new Order("Товар 4","В обработке",4,"20.07.25",120,"Игрушки"));
+        orders.add(new Order("Товар 5","Получен",5,"13.06.25",150,"Книги"));
 
         List<Order> juniOrder = orders.stream().filter(p -> p.getDate().substring(3,5).contains("06")).collect(Collectors.toList());
         System.out.println( "Заказы в июне : " + juniOrder);
