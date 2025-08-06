a65d43232d0eeeed029a261aab13f6661f115e6c
SergeyGrechikhin <blackblock456@gmail.com>
2025-08-06 22:14:03 +0200

    16 Homework

--- src/javapro/homework16/task3/CsvFile.java
+++ src/javapro/homework16/task3/CsvFile.java
@@ -9,10 +9,11 @@ public class CsvFile {
     public static void main(String[] args) {
         CsvFileService csvFileService = new CsvFileService();
 
+
         String fileName = "src/javapro/homework16/task3/file1.csv" ;
+        int mylines = 0 ;
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            int mylines = 0 ;
             while ((line = br.readLine()) != null) {
                 if(line.length() == 0)
                     continue;
@@ -29,9 +30,15 @@ public class CsvFile {
                     }
 
             }
+            System.out.println("Quantity lines : " + mylines);
+
         }catch (Exception e){
-            System.out.println("Error");
+            System.out.println("File not found or format not suitable");
         }
+
+
+
+
 
 
 
